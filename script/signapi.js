110e44a1e1af45fc3584aafc9f8a4d64c42e8c08
Roshu2 <kyumin1020@gmail.com>
2022-05-19 15:25:23 +0900

    1. 기본와이어프레임 2. 로그인회원가입 기능 뼈대연결완료

---
+++ script/signapi.js
@@ -0,0 +1,49 @@
+const backend_base_url = "http://127.0.0.1:5000"
+const frontend_base_url = "http://127.0.0.1:5500"
+
+// 회원가입 함수
+async function handleSignup(){
+
+    const signupData = {
+        username : document.getElementById("username").value,
+        userid : document.getElementById("userid").value,
+        password : document.getElementById("userpw").value
+    }
+    const response = await fetch(`${backend_base_url}/signup`,{
+        method:'POST',
+        body:JSON.stringify(signupData)
+    }
+    )
+    response_json = await response.json()
+    
+    if (response.status == 200){
+        window.location.replace(`${frontend_base_url}/login.html`);
+    }else {
+        alert(response.status)
+    }
+}
+
+
+//로그인 함수
+async function handleLogin(){
+    const loginData = {
+        userid : document.getElementById("userid").value,
+        password : document.getElementById("userpw").value
+    }
+    
+    const response = await fetch(`${backend_base_url}/login`,{
+        method:'POST',
+        body:JSON.stringify(loginData)
+    }
+    )
+    response_json = await response.json()
+    
+    localStorage.setItem("token", response_json.token)
+
+    if (response.status == 200){
+        window.location.replace(`${frontend_base_url}/index.html`);
+    }else {
+        alert(response.status)
+    }
+}
+
