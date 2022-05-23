30585536ae98cf4ac08aa669820a3aecf835aaff
Roshu2 <kyumin1020@gmail.com>
2022-05-23 18:07:34 +0900

    1. 카카오 로그인 진행중 2. js 업로드 프론트 작성

--- script/mainapi.js
+++ script/mainapi.js
@@ -1,3 +1,31 @@
+/* POST 요청 ajax 코드 */
+function uploadRecycle() {
+    // 고유 id let 함수로 정의
+    let today = new Date().toISOString()
+    let image = $('#chooseFile')[0].files[0]
+    let form_data = new FormData()
+
+
+    form_data.append("image_give", image)
+    form_data.append("date_give", today)
+
+    $.ajax({
+        type: "POST",
+        url: "/upload",
+        data: form_data,
+        cache: false,
+        contentType: false,
+        processData: false,
+        success: function (response) {
+            alert(response['msg'])
+            window.location.reload()
+        }
+    })
+}
+
+
+
+
 function getuserpaper() {
     $.ajax({
         type: 'GET',
